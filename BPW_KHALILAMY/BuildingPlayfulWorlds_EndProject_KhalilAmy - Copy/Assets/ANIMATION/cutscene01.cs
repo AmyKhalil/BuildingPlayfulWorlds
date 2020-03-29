a2597004991149310282579a1e1b8358145bb617
AmyKhalil <acmkhy@gmail.com>
2020-03-29 16:20:11 +0200

    Cutscene

---
+++ BPW_KHALILAMY/BuildingPlayfulWorlds_EndProject_KhalilAmy - Copy/Assets/ANIMATION/cutscene01.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutscene01 : MonoBehaviour
+{
+    public GameObject thePlayer; 
+    public GameObject cutSceneCam; 
+
+    void OntriggerEnter(Collider other)
+    {
+    if (other.gameObject.CompareTag("Player")) { 
+        cutSceneCam.SetActive(true);
+        thePlayer.SetActive(false);
+
+        Debug.Log("collision detected");
+    }
+    }
+        
+}
